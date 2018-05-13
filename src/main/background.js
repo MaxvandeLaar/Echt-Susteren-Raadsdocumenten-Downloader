ee7fc74c326a96bc250f17fa5463d261c7a7290d
Max van de Laar <max.vandelaar@me.com>
2018-05-13 13:08:23 +0200

    Add logging to updater

--- src/main/background.js
+++ src/main/background.js
@@ -5,12 +5,16 @@ import {devMenuTemplate} from '../menu/dev_menu_template';
 import {editMenuTemplate} from '../menu/edit_menu_template';
 import createWindow from '../helpers/window';
 import './listeners';
+const log = require('electron-log');
 import {autoUpdater} from 'electron-updater';
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
 import env from 'env';
 import provider from 'provider';
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 const setApplicationMenu = () => {
     const menus = [editMenuTemplate];
@@ -28,9 +32,35 @@ if (env.name !== 'production') {
     app.setPath('userData', `${userDataPath} (${env.name})`);
 }
 
+autoUpdater.on('checking-for-update', () => {
+    log.info('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+    log.info('Update available.');
+});
+autoUpdater.on('update-not-available', (info) => {
+    log.info('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+    log.info('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    log.info(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+    log.info('Update downloaded');
+});
+
 app.on('ready', () => {
-    autoUpdater.setFeedURL(provider);
-    autoUpdater.checkForUpdatesAndNotify();
+    // autoUpdater.setFeedURL(provider);
+    autoUpdater.checkForUpdates().then((value) => {
+        log.info(value);
+    }, (reason) => {
+        log.error(reason);
+    });
     setApplicationMenu();
 
     const mainWindow = createWindow('main', {
